550d26ae08711a1e921d3e9504d317a146dff77e
SaminthaKrizan <72949301+SaminthaKrizan@users.noreply.github.com>
2020-10-16 15:25:49 -0500

    First test from Sami's computer

--- Monopoly Game/Assets/Scripts/Game.cs
+++ Monopoly Game/Assets/Scripts/Game.cs
@@ -5,6 +5,7 @@
 //########## Next To Do - Work on M Loop ##########// 
 //########## Level 1 - Sami's Desktop @ Home ##########//
 //########## 10/16/2020 Test Add By RK, Sami & Jack ##########// 
+//########## 10/16/2020 Test Add from Sami's computer ##########//
 
 using System.Collections;
 using System.Collections.Generic;
