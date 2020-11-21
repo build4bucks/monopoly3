eff7246255e1914c7846fe6e7bb75a82a2b66103
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2020-11-20 18:32:40 -0600

    SNL v3
    SNL v3

---
+++ Monopoly Game/Assets/Scripts/SNL2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SNL
+{
+
+    public class SNL2 : MonoBehaviour
+    {
+        public static string thePlayers;
+        public GameObject inputField;
+        public GameObject textDisplay;
+        public void StoreName()
+        {
+            thePlayers = inputField.GetComponent<Text>().text;
+            textDisplay.GetComponent<Text>().text = thePlayers + " Players";
+        }
+
+    }
+}
